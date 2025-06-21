5fdbd53ad8e081a98646dfdd7649a42a7bc526a8
andyle200330 <khoavyvn90504@gmail.com>
2025-06-21 21:59:20 +0700

    add gui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-     public static void main(String[] args) {
-        System.out.println("hello");
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main (){
+        setTitle("Chat");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 }
