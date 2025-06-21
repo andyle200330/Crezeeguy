57e74336894c70faa9315948f1b47fbb0c722cef
andyle200330 <khoavyvn90504@gmail.com>
2025-06-21 22:16:00 +0700

    button

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
+    private JLabel jsfirstname;
+    private JTextField Jschat;
+    private JButton Jbutton;
+    private JPanel MainPanel;
+
     public Main (){
+        setContentPane(MainPanel);
         setTitle("Chat");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(300,200);
         setLocationRelativeTo(null);
         setVisible(true);
 
+        Jbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(Main.this, "testing button");
+            }
+        });
     }
 
     public static void main(String[] args) {
