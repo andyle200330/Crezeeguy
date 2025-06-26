6cb972e3038ef79a29b93f8401d91104742b32c6
andyle200330 <khoavyvn90504@gmail.com>
2025-06-26 16:52:42 +0700

    test chat sending

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,14 @@ public class Main extends JFrame {
     private JButton Jbutton;
     private JPanel MainPanel;
 
-    public Main (){
+    private ObjectOutputStream out;  // Gửi dữ liệu
+
+
+
+    public Main (ObjectOutputStream outStream){
+
+        this.out = outStream;
+
         setContentPane(MainPanel);
         setTitle("Chat");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -22,15 +29,26 @@ public class Main extends JFrame {
         Jbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(Main.this, "testing button");
+                String message = Jschat.getText();
+
+                try {
+                    out.writeObject(message);
+                    out.flush();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                Jschat.setText("");
             }
         });
     }
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost",8888);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        new Main();
+
+        new Main(out);
 
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -2,11 +2,20 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         ServerSocket serverSocket = new ServerSocket(8888);
+
         Socket socket = serverSocket.accept();
 
         System.out.println("client connect!!!");
+
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+        while(true){
+            String message = (String) in.readObject();
+            System.out.println("client: "+message);
+
+        }
 
    }
 
